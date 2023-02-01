981c5cf25f26964a0fa22a74f524ba4a4dc6c710
vosvosvos <indvfx@gmail.com>
2023-02-01 23:14:47 +0100

    Comments

--- Hermes.Application/Services/HttpRequestService.cs
+++ Hermes.Application/Services/HttpRequestService.cs
@@ -12,6 +12,13 @@ namespace Hermes.Application.Services
             Http = client;
         }
 
+        /// <summary>
+        /// Post request to the server with the given model
+        /// Credentials is needed for the Set-Cookie response header (if it's not set, the cookie will not be saved)
+        /// </summary>
+        /// <param name="model">DTO or ViewModel</param>
+        /// <param name="url">URL to post to</param>
+        /// <returns>HttpResponseMessage</returns>
         public async Task<HttpResponseMessage> PostWithCredentials(object model, string url)
         {
             var request = new HttpRequestMessage(HttpMethod.Post, url);
@@ -21,18 +28,35 @@ namespace Hermes.Application.Services
             return response;
         }
 
+        /// <summary>
+        /// Post request to the server with the given model
+        /// </summary>
+        /// <param name="model">DTO or ViewModel</param>
+        /// <param name="url">URL to post to</param>
+        /// <returns>HttpResponseMessage</returns>
         public async Task<HttpResponseMessage> Post(object model, string url)
         {
             var response = await Http.PostAsJsonAsync(url, model);
             return response;
         }
 
+        /// <summary>
+        /// GET request to server
+        /// </summary>
+        /// <param name="url">URL</param>
+        /// <returns>HttpResponseMessage</returns>
         public async Task<HttpResponseMessage> Get(string url)
         {
             var response = await Http.GetAsync(url);
             return response;
         }
 
+        /// <summary>
+        /// PUT request
+        /// </summary>
+        /// <param name="model">DTO or ViewModel</param>
+        /// <param name="url"></param>
+        /// <returns>HttpResponseMessage</returns>
         public async Task<HttpResponseMessage> Put(object model, string url)
         {
             var response = await Http.PutAsJsonAsync(url, model);
